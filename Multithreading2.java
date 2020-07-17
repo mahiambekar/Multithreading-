d51460fb77229046067d7d1c610ce8d805b55037
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-17 18:11:51 +0530

    Add files via upload

---
+++ Multithreading2.java
@@ -0,0 +1,46 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+class Test implements Runnable{
+    Thread t;
+    Test(String n){
+        t=new Thread(this);
+        t=setName(n);
+        t.start();
+    }
+    @Override
+    public void run(){
+        int i;
+        try{
+            for(i=0;i<5;i++){
+                System.out.println("Child Thread"+t.getName()+":"+i);
+                Thread.sleep(500);
+            }
+        }catch(Exception e){
+            
+        }
+        
+    }
+}
+public class Main
+{
+	public static void main(String[] args)throws Exception {
+	    
+		Test t1=new Test("A");
+		Test t2=new Test("B");
+		Test t3=new Test("C");
+		int i;
+		try{
+		    for(i=0;i<5;i++){
+		        System.out.println("Main thread"+i);
+		        Thread.sleep(1500);
+		    }
+		}catch(Exception e){
+		    
+		}
+	}
+}
